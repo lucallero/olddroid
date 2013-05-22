48b2f271a21431168a1f4b34491fa6eb44dcec02
lcallero <lucianocallero@gmail.com>
2013-05-22 18:40:22 -0300

    Incluído cálculo do tempo da volta, média, pior volta e melhor volta.

--- SStopwatch/src/com/jogapc/sstopwatch/StopwatchThread.java
+++ SStopwatch/src/com/jogapc/sstopwatch/StopwatchThread.java
@@ -1,8 +1,10 @@
 package com.jogapc.sstopwatch;
 
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.apache.commons.lang3.time.StopWatch;
 
 import android.app.Activity;
@@ -10,64 +12,87 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class StopwatchThread extends Thread {
-	private Logger logger;
-	private boolean run;
-	private StopWatch stopWatch;
-	private TextView chronometerTextView;
-	LinearLayout splitTimeContainer;
-	private int layoutId;
-	private Activity mainActivity;
+    private Logger logger;
+    private boolean run;
+    private StopWatch stopWatch;
+    private TextView chronometerTextView;
+    private LinearLayout splitTimeContainer;
+    private int layoutId;
+    private Activity mainActivity;
+    private long lapAverage = 0L;
+    private long lastSplitTime = 0L;
+    private long lapTime = 0L;
+    private long bestLap = 0L;
+    private long worstLap = 0L;
+    private int lapCount = 0;
+    private ArrayList<Long> splitTimeList = new ArrayList<Long>();
 
-	public StopwatchThread(TextView textViewMainTime,
-			LinearLayout aSplitTimeContainer, int aLayoutId,
-			Activity aMainActivity) {
-		this.logger = Logger.getLogger(getClass().getName());
-		this.run = true;
-		this.stopWatch = new StopWatch();
-		this.chronometerTextView = textViewMainTime;
-		this.splitTimeContainer = aSplitTimeContainer;
-		this.layoutId = aLayoutId;
-		this.mainActivity = aMainActivity;
+    public StopwatchThread(TextView textViewMainTime, LinearLayout aSplitTimeContainer,
+	    int aLayoutId, Activity aMainActivity) {
+	this.logger = Logger.getLogger(getClass().getName());
+	this.run = true;
+	this.stopWatch = new StopWatch();
+	this.chronometerTextView = textViewMainTime;
+	this.splitTimeContainer = aSplitTimeContainer;
+	this.layoutId = aLayoutId;
+	this.mainActivity = aMainActivity;
+    }
+
+    public void stopWork() {
+	this.run = false;
+    }
+
+    public boolean isRunning() {
+	return this.run;
+    }
+
+    /**
+     * Calcula e atualiza as variveis de tempo, tempo da volta, melhor volta,
+     * pior volta, media das voltas
+     * 
+     * @param aTextView
+     */
+    public void splitTime(final TextView aTextView) {
+	this.stopWatch.split();
+	this.lapTime = this.stopWatch.getSplitTime() - this.lastSplitTime;
+	this.lastSplitTime = this.stopWatch.getSplitTime();
+	if (this.lapTime < this.bestLap) {
+	    this.bestLap = this.lapTime;
+	}
+	if (this.lapTime > this.worstLap) {
+	    this.worstLap = this.lapTime;
 	}
 
-	public void stopWork() {
-		this.run = false;
-	}
+	this.lapAverage = stopWatch.getSplitTime() / ++this.lapCount;
 
-	public boolean isRunning() {
-		return this.run;
-	}
+	aTextView
+		.setText(this.lapCount + " - " + DurationFormatUtils.formatDurationHMS(this.lapTime));
+	this.mainActivity.runOnUiThread(new Runnable() {
+	    @Override
+	    public void run() {
+		splitTimeContainer.addView(aTextView);
+	    }
+	});
+    }
 
-	public void splitTime(final TextView aTextView) {
-		stopWatch.split();
-		aTextView.setText(stopWatch.toSplitString());
-		mainActivity.runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				splitTimeContainer.addView(aTextView);
-			}
+    @Override
+    public void run() {
+	this.stopWatch.start();
+	while (run) {
+	    try {
+		Thread.sleep(50);
+		this.mainActivity.runOnUiThread(new Runnable() {
+		    @Override
+		    public void run() {
+			chronometerTextView.setText(stopWatch.toString());
+		    }
 		});
+	    } catch (InterruptedException e) {
+		this.logger.log(Level.SEVERE, e.getMessage());
+		e.printStackTrace();
+	    }
 	}
-
-	@Override
-	public void run() {
-		stopWatch.start();
-		while (run) {
-			try {
-				Thread.sleep(50);
-				mainActivity.runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						chronometerTextView.setText(stopWatch.toString());
-					}
-				});
-//				System.out.println(layoutId + ":" + stopWatch.toString());
-			} catch (InterruptedException e) {
-				logger.log(Level.SEVERE, e.getMessage());
-				e.printStackTrace();
-			}
-		}
-		stopWatch.reset();
-		logger.log(Level.INFO, layoutId + "Encerrou");
-	}
+	this.stopWatch.reset();
+	this.logger.log(Level.INFO, layoutId + "Encerrou");
+    }
 }
