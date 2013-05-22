db7a22a739d47b2b11adc2fbff05c676ad08b55c
luciano <lucianocallero@gmail.com>
2013-05-21 21:08:15 -0300

    Scroll view.

--- SStopwatch/src/com/jogapc/sstopwatch/LaneLayout.java
+++ SStopwatch/src/com/jogapc/sstopwatch/LaneLayout.java
@@ -4,59 +4,73 @@ import android.content.Context;
 import android.graphics.Color;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class LaneLayout extends LinearLayout {
 
-	private TextView textViewChronometer;
-	private TextView textViewLane;
-	private Button buttonLap;
-	private ToggleButton toggleButton;
+    private TextView textViewChronometer;
+    private TextView textViewLane;
+    private Button buttonLap;
+    private ToggleButton toggleButton;
+    private ScrollView scrollSplit;
+    private LinearLayout scrollFrame;
 
-	public LaneLayout(Context context) {
-		super(context);
-	}
+    public LaneLayout(Context context) {
+	super(context);
+    }
 
-	public LaneLayout(Context context, int id, int backgroundColor) {
-		super(context);
-		this.setId(id);
-		this.setOrientation(LinearLayout.VERTICAL);
-		this.setBackgroundColor(backgroundColor);
-		this.textViewChronometer = new TextView(context);
-		this.textViewLane = new TextView(context);
-		this.buttonLap = new Button(context);
-		this.toggleButton = new ToggleButton(context);
-		this.textViewChronometer.setText("0:00:00:000");
-		this.textViewChronometer.setTextColor(Color.BLUE);
-		this.textViewLane.setText("new Lane");
-		this.buttonLap.setText("Lap");
-		this.toggleButton.setText("Start");
-		this.toggleButton.setTextOff("Start");
-		this.toggleButton.setTextOn("Stop");
-		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-				LinearLayout.LayoutParams.WRAP_CONTENT,
-				LinearLayout.LayoutParams.WRAP_CONTENT, 2);
-		this.addView(textViewChronometer, params);
-		this.addView(buttonLap);
-		this.addView(toggleButton);
-		this.addView(textViewLane);
+    public LaneLayout(Context context, int id, int backgroundColor) {
+	super(context);
+	LinearLayout.LayoutParams scrollLayoutParams = new LinearLayout.LayoutParams(
+		LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 2);
+	this.setId(id);
+	this.setOrientation(LinearLayout.VERTICAL);
+	this.setBackgroundColor(backgroundColor);
+	this.buttonLap = new Button(context);
+	this.buttonLap.setText("Lap");
+	this.textViewChronometer = new TextView(context);
+	this.textViewChronometer.setText("0:00:00:000");
+	this.textViewChronometer.setTextColor(Color.BLUE);
+	this.scrollSplit = new ScrollView(context);
+	this.textViewLane = new TextView(context);
+	this.textViewLane.setText("new Lane");
+	this.toggleButton = new ToggleButton(context);
+	this.toggleButton.setText("Start");
+	this.toggleButton.setTextOff("Start");
+	this.toggleButton.setTextOn("Stop");
+	this.scrollSplit = new ScrollView(context);
+	this.scrollSplit.setVerticalScrollBarEnabled(true);
+	this.scrollFrame = new LinearLayout(context);
+	this.scrollFrame.setOrientation(LinearLayout.VERTICAL);
+	this.addView(textViewChronometer);
+	this.addView(scrollSplit, scrollLayoutParams);
+	this.scrollSplit.addView(scrollFrame);
+	for (int i = 0; i < 30; i++) {
+	    TextView lapText = new TextView(context);
+	    lapText.setText("lap " + i);
+	    scrollFrame.addView(lapText);
 	}
+	this.addView(buttonLap);
+	this.addView(toggleButton);
+	this.addView(textViewLane);
+    }
 
-	public TextView getTextViewChronometer() {
-		return textViewChronometer;
-	}
+    public TextView getTextViewChronometer() {
+	return textViewChronometer;
+    }
 
-	public TextView getTextViewLane() {
-		return textViewLane;
-	}
+    public TextView getTextViewLane() {
+	return textViewLane;
+    }
 
-	public Button getButtonLap() {
-		return buttonLap;
-	}
+    public Button getButtonLap() {
+	return buttonLap;
+    }
 
-	public ToggleButton getToggleButton() {
-		return toggleButton;
-	}
+    public ToggleButton getToggleButton() {
+	return toggleButton;
+    }
 
 }
--- SStopwatch/src/com/jogapc/sstopwatch/StopwatchThread.java
+++ SStopwatch/src/com/jogapc/sstopwatch/StopwatchThread.java
@@ -9,48 +9,47 @@ import android.app.Activity;
 import android.widget.TextView;
 
 public class StopwatchThread extends Thread {
-	private Logger logger;
-	private boolean run;
-	private StopWatch stopWatch;
-	private TextView chronometerTextView;
-	private int layoutId;
-	private Activity mainActivity;
+    private Logger logger;
+    private boolean run;
+    private StopWatch stopWatch;
+    private TextView chronometerTextView;
+    private int layoutId;
+    private Activity mainActivity;
 
-	public StopwatchThread(TextView aTimeLabel, int aLayoutId,
-			Activity aMainActivity) {
-		this.logger = Logger.getLogger(getClass().getName());
-		this.run = true;
-		this.stopWatch = new StopWatch();
-		this.chronometerTextView = aTimeLabel;
-		this.layoutId = aLayoutId;
-		this.mainActivity = aMainActivity;
+    public StopwatchThread(TextView aTimeLabel, int aLayoutId, Activity aMainActivity) {
+	this.logger = Logger.getLogger(getClass().getName());
+	this.run = true;
+	this.stopWatch = new StopWatch();
+	this.chronometerTextView = aTimeLabel;
+	this.layoutId = aLayoutId;
+	this.mainActivity = aMainActivity;
+    }
+
+    public void stopWork() {
+	this.run = false;
+    }
+
+    @Override
+    public void run() {
+	stopWatch.start();
+	while (run) {
+	    try {
+		Thread.sleep(50);
+		mainActivity.runOnUiThread(new Runnable() {
+		    @Override
+		    public void run() {
+			chronometerTextView.setText(stopWatch.toString());
+		    }
+		});
+
+		System.out.println(layoutId + ":" + stopWatch.toString());
+	    } catch (InterruptedException e) {
+		logger.log(Level.SEVERE, e.getMessage());
+		e.printStackTrace();
+	    }
 	}
+	stopWatch.reset();
+	logger.log(Level.INFO, layoutId + "Encerrou");
+    }
 
-	public void stopWork() {
-		this.run = false;
-	}
-
-	@Override
-	public void run() {
-		stopWatch.start();
-		while (run) {
-			try {
-				Thread.sleep(10);
-				mainActivity.runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						chronometerTextView.setText(stopWatch.toString());
-					}
-				});
-
-				System.out.println(layoutId + ":" + stopWatch.toString());
-			} catch (InterruptedException e) {
-				logger.log(Level.SEVERE, e.getMessage());
-				e.printStackTrace();
-			}
-		}
-		stopWatch.reset();
-		logger.log(Level.INFO, layoutId + "Encerrou");
-	}
-	
 }
